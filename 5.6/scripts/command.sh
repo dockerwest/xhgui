989b929cce3f98516bc86f721ca0819d5a94bf28
BlackEagle <ike.devolder@gmail.com>
2017-02-22 20:33:08 +0100

    make sure /xhprof is owned by www-data
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- 5.6/scripts/command.sh
+++ 5.6/scripts/command.sh
@@ -2,6 +2,10 @@
 
 /prepare.sh
 
+# dont count on the DEVELOPMENT flag
+mkdir -p /xhprof
+chown www-data:www-data /xhprof
+
 gosu www-data rsync -a --info=progress2 /var/lib/xhgui/ /phpapp/
 
 if [ "php-fpm5.6" != "$1" ] && [ "/bin/bash" != "$1" ]; then
