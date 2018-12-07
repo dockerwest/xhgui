2d1ee1ae7b347d58dd5888d318536e97642cd193
BlackEagle <ike.devolder@gmail.com>
2018-12-07 17:21:33 +0100

    add php 7.3
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

---
+++ 7.3/scripts/install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+# install packages
+apt-get update
+apt-get install -y rsync git
+
+extensions -i xml mcrypt mongodb
+
+mkdir -p /var/lib/xhgui
+chown www-data: /var/lib/xhgui
+cd /var/lib/xhgui
+gosu www-data git clone https://github.com/perftools/xhgui.git .
+gosu www-data /usr/local/lib/composer install
+sed -e 's/127\.0\.0\.1/mongo/g' -i config/config.default.php
+
+apt-get purge -y --auto-remove git
+
+apt-get clean -y
+
+# update permissions to allow rootless operation
+/usr/local/bin/permissions
